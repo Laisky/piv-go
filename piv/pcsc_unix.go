4d8734c62d254f7ff77991f9448c3a68ff01c3e8
Laisky <github@laisky.com>
2023-09-21 06:39:22 +0000

    build: Refactor build constraint to support additional architectures
    - Update build constraint to include `amd64` and `arm64` architectures in `piv/pcsc_unix.go` file

--- piv/pcsc_unix.go
+++ piv/pcsc_unix.go
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build darwin || linux || freebsd || openbsd
-// +build darwin linux freebsd openbsd
+//go:build darwin || linux || freebsd || openbsd || amd64 || arm64
+// +build darwin linux freebsd openbsd amd64 arm64
 
 package piv
 
